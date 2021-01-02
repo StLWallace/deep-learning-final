0d3cce20136a1db8f04eda74052e961f47d0ef74
Stephen Wallace <stephen.wallace@wustl.edu>
2021-01-02 18:00:27 +0000

    Updated comments in run_training script

--- run_training.sh
+++ run_training.sh
@@ -1,3 +1,7 @@
+# To run this, change the job-dir to your GCS location
+# Set glove_path to location of your glove.6B.100d.txt file
+# Set output_path to the GCS location where you want the models to be output
+# Set staging bucket to wherever you want the training package to be staged
 NOW=$( date '+%Y%m%d%H%M' )
 gcloud ai-platform jobs submit training sw_deep_learning_training_$NOW \
     --staging-bucket=gs://slalom-stl-kaggle-datasets \
@@ -7,7 +11,6 @@ gcloud ai-platform jobs submit training sw_deep_learning_training_$NOW \
     --region=us-central1 \
     --runtime-version='2.3' \
     --python-version='3.7' \
-    --scale-tier=BASIC_GPU \
     -- \
     --glove_path=gs://slalom-stl-kaggle-datasets/fake-comments/glove.6B.100d.txt \
     --output_path=gs://slalom-stl-kaggle-datasets/fake-comments/results
